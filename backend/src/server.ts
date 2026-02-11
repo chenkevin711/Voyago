142618fb1d8a52d6cf7b5f6736855aa058c6217d
Patrick Zheng <pz92@drexel.edu>
2026-02-11 16:32:15 -0500

    inital database setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import { connectToDatabase, closeDatabaseConnection } from './config/database'
 
 const app = express()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// Load environment variables from the .env file
+const { loadEnvFile } = require('node:process');
+loadEnvFile();
+
 
 // Test route
 app.get('/api/health', (req: Request, res: Response) => {
@@ -17,3 +23,17 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 5000
 app.listen(PORT, () => {
     console.log(`API listening on http://localhost:${PORT}`)
 })
+
+// Connect to DB
+connectToDatabase();
+
+// Graceful Shutdown Handlers
+process.on("SIGINT", () => {
+    closeDatabaseConnection();
+    process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+    closeDatabaseConnection();
+    process.exit(0);
+});
